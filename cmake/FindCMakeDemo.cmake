6c5e02091f31c841db46c71dc85cd9b879fda7c3
Matt Morse <mmorse1217@gmail.com>
2020-06-14 18:25:09 -0400

    adding comments to cmake/FindCMakeDemo.cmake

--- cmake/FindCMakeDemo.cmake
+++ cmake/FindCMakeDemo.cmake
@@ -10,27 +10,44 @@ if(CMakeDemo_FOUND)
     return()
 endif()
 
-# We favor libraries installed in /usr/local with the prefix .../CMakeDemo-*, so
-# we make a list of 
+# We prioritize libraries installed in /usr/local with the prefix .../CMakeDemo-*, 
+# so we make a list of them here
 file(GLOB lib_glob "/usr/local/lib/CMakeDemo-*")
 file(GLOB inc_glob "/usr/local/include/CMakeDemo-*")
 
+# Find the library with the name "CMakeDemo" on the system. Store the final path
+# in the variable CMakeDemo_LIB
 find_library(CMakeDemo_LIB 
+    # The library is named "CMakeDemo", but can have various library forms, like
+    # libCMakeDemo.a, libCMakeDemo.so, libCMakeDemo.so.1.x, etc. This should
+    # search for any of these.
     NAMES CMakeDemo
+    # Provide a list of places to look based on prior knowledge about the system.
+    # We want the user to override /usr/local with environment variables, so
+    # this is included here.
     HINTS
         ${CMakeDemo_DIR}
         ${CMAKEDEMO_DIR}
         $ENV{CMakeDemo_DIR}
         $ENV{CMAKEDEMO_DIR}
         ENV CMAKEDEMO_DIR
+    # Provide a list of places to look as defaults. /usr/local shows up because
+    # that's the default install location for most libs. The globbed paths also
+    # are placed here as well.
     PATHS
         /usr
         /usr/local
         /usr/local/lib
         ${lib_glob}
+    # Constrain the end of the full path to the detected library, not including
+    # the name of library itself.
     PATH_SUFFIXES 
         lib
 )
+
+# Find the path to the file "source_file.hpp" on the system. Store the final
+# path in the variables CMakeDemo_INCLUDE_DIR. The HINTS, PATHS, and
+# PATH_SUFFIXES, arguments have the same meaning as in find_library().
 find_path(CMakeDemo_INCLUDE_DIR source_file.hpp
     HINTS
         ${CMakeDemo_DIR}
@@ -48,27 +65,33 @@ find_path(CMakeDemo_INCLUDE_DIR source_file.hpp
 )
 
 
+# Check that both the paths to the include and library directory were found.
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(CMakeDemo
     "\nCMakeDemo not found --- You can download it using:\n\tgit clone 
     https://github.com/mmorse1217/cmake-project-template\n and setting the CMAKEDEMO_DIR environment variable accordingly"
     CMakeDemo_LIB CMakeDemo_INCLUDE_DIR)
+
+# These variables don't show up in the GUI version of CMake. Not required but
+# people seem to do this...
 mark_as_advanced(CMakeDemo_INCLUDE_DIR CMakeDemo_LIB)
 
+# Finish defining the variables specified above. Variables names here follow
+# CMake convention.
 set(CMakeDemo_INCLUDE_DIRS ${CMakeDemo_INCLUDE_DIR})
 set(CMakeDemo_LIBRARIES ${CMakeDemo_LIB})
-#set(CMakeDemo_LIBRARIES "${CMakeDemo_LIB}/../lib/libblendsurf.a"  )
-#set(CMakeDemo_render_LIBRARIES "${CMakeDemo_LIB}/../lib/libblendsurf_render.a"  )
-#set(CMakeDemo_render_INCLUDE_DIR "${CMakeDemo_LIB}/../vis/"  )
 
+# If the above CMake code was successful and we found the library, and there is
+# no target defined, lets make one.
 if(CMakeDemo_FOUND AND NOT TARGET CMakeDemo::CMakeDemo)
     add_library(CMakeDemo::CMakeDemo UNKNOWN IMPORTED)
-    # Interface include directory
+    # Set location of interface include directory, i.e., the directory
+    # containing the header files for the installed library
     set_target_properties(CMakeDemo::CMakeDemo PROPERTIES
         INTERFACE_INCLUDE_DIRECTORIES "${CMakeDemo_INCLUDE_DIRS}"
         )
 
-    # Link to library file
+    # Set location of the installed library
     set_target_properties(CMakeDemo::CMakeDemo PROPERTIES
         IMPORTED_LINK_INTERFACE_LANGUAGES "CXX"
         IMPORTED_LOCATION "${CMakeDemo_LIBRARIES}"
